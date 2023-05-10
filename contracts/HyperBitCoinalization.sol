e5e70e3ff5385fcb06fd4fc36e94c94e19dc944b
Arefeh Ahmadi <arefeh@debian-BULLSEYE-live-builder-AMD64>
2023-05-10 16:44:35 +0330

    add set winner function

--- contracts/HyperBitCoinalization.sol
+++ contracts/HyperBitCoinalization.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
+import "openzeppelin-contracts/utils/math/Math.sol";
 // import "hardhat/console.sol";
 
 interface Oracle {
@@ -38,7 +39,6 @@ contract HyperBitCoinalization {
 
 	address winnerToken;
 
-	// duration is in form of days
 	constructor(address _btc, address _usdc, address _oracle, uint _conversionRatio, uint _endTimeStamp) {
 		btc = _btc;
 		usdc = _usdc;
@@ -83,7 +83,50 @@ contract HyperBitCoinalization {
 
 	}
 
-		
+	function btcInBet() public returns (uint256) {
+		// TODO: error handling
+		uint256 _btc = Math.min(btcTotal, Math.div(usdcTotal, conversionRatio));
+		return binarySearch(_btc, btcAcc[msg.sender]);
+	}
 
-	
+	function usdcInBet() public returns (uint256) {
+		// TODO: error handling
+		uint256 _usdc = Math.min(usdcTotal, Math.mul(usdcTotal, conversionRatio));
+		return binarySearch(_usdc, usdcAcc[msg.sender]);
+	}
+
+	function binarySearch(uint256 total, Acc[] acc) internal returns (uint 256){
+		uint len = acc.length;
+		uint start = 0;
+		uint end = len - 1;
+
+		if(len == 0) return 0;
+
+		while(start < end){
+			uint mid = (start + end)/2;
+			
+			if(acc[mid].tokenTotal - acc[mid].amount < total){
+				start = mid + 1;
+			}else{
+				end = mid;
+			}
+		}
+
+		if(acc[start].tokenTotal <= total){
+			return acc[start].userTotal;
+		}
+		uint diff = total - acc[start].tokenTotal;
+		return acc[start].userTotal - diff;
+	}
+
+	function setWinnerToken() public {
+		if(winnerToken != address(0)) revert;
+		
+		uint8 decimals = Oracle(oracle).decimals();
+		uint256 answer = Oracle(oracle).latestAnswer();
+		uint256 treshold = conversionRatio * 10 ** decimals;
+
+		if(answer >= treshold) winnerToken = btc;
+		else if(block.timestamp >= endTimeStamp) winnerToken = usdc;
+	}
 }
